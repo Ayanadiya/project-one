75a08c7b865aae6a85f6491307e605b77d5c0416
ayanadiya <ayanadiya17@gmail.com>
2025-04-28 14:46:13 +0530

    Learned to use useState hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ExpenseItem.css'
 import './ExpenseDate';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 function ExpenseItem(props){
-  function buttonHandler(event){
-    console.log("Button clicked");
+  const [title, setTitle]=useState(props.title)
+  const buttonHandler= ()=>{
+    setTitle("New Title");
   }
   return(
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__location">{props.location}</div>
       <div>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.price}</div>
       </div> 
       <button onClick={buttonHandler}>Change Title</button>
